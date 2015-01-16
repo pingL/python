f0002ea97f393ee8c1ed3901691ddb624ee950f9
Atharv Sonwane <manyogiee@gmail.com>
2015-01-16 19:06:19 +0000

    Update Fermat's Little Theorem Prime Checker.py

--- prime-numbers/Fermat's Little Theorem Prime Checker.py
+++ prime-numbers/Fermat's Little Theorem Prime Checker.py
@@ -5,6 +5,9 @@
 import random
 print("Welcome to Fermat's Little Theorem Prime Checker\n")
 p=int(input("Which number would you like to test for primality? "))
+if p == 1:
+    print("1 is NOT PRIME, due to your foolishness this program will exit")
+    sys.exit(1)
 print("")
 n=1
 cm=(561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341, 41041, 46657, 52633, 62745, 63973, 75361, 101101, 115921, 126217, 162401, 172081, 188461, 252601, 278545, 294409, 314821, 334153, 340561, 399001, 410041, 449065, 488881, 512461)
