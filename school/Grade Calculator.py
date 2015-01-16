7a1d4ad9a600a101d37e57250ef4f5d6323c986f
Atharv Sonwane <manyogiee@gmail.com>
2015-01-16 19:26:44 +0000

    Update Grade Calculator.py

--- school/Grade Calculator.py
+++ school/Grade Calculator.py
@@ -8,22 +8,25 @@ import sys
 repeat = True
 while repeat == True:
 
-    print("THE SUPREME GRADE CALCULATING BEING\n")
-    sys.stdout.write('.') 
+    print("\nTHE SUPREME GRADE CALCULATING BEING")
+    i=0
+    while i < 4:
+        sys.stdout.write('.') 
         sleep(0.5)
         i += 1
-    mark = input("Enter your mark: ")
-    print("")
-    sys.stdout.write('.') 
+    mark = int(input("\nEnter your mark: "))
+    
+    while i < 9:
+        sys.stdout.write('.')
         sleep(0.5)
         i += 1
         
     def report(mark, grade, comment):
         print("Your mark: " + str(mark) + ". Your grade: " + grade + ". Comment: " +
         comment)
-
+    print("")
     if mark >= 81:
-        garde = "A"
+        grade = "A"
         report(mark, grade, "Aim for an A*.")
     elif mark >= 75:
         grade = "B"
@@ -36,20 +39,20 @@ while repeat == True:
         report(mark, grade, "Very Bad. I expect much more from you")
     elif mark >= 58:
         grade = "E"
-        print(mark, grade, "Absolute rubbish. How can you get so bad? That is appauling")
+        report(mark, grade, "Absolute rubbish. How can you get so bad? That is appauling")
     elif mark < 58:
         grade = "U"
-        print(mark, grade, "Get out of my sight.")
+        report(mark, grade, "Get out of my sight.")
 
-       repeatstr = input("Do you want to go again? BEWARE! [Y/n] ")
-        if repeatstr == "Y" or repeatstr == "y" or repeatstr == '':
-            repeat = True
-        elif repeatstr == "N" or repeatstr == "n":
-            repeat = False
-        elif repeatstr == "penguins":
-            print("Penguins are cool! Now, for your answer to the question...")
-        else:
-            print("Give a proper answer or FELL MY WRATH\n")
+    repeatstr = input("\nDo you want to go again? BEWARE! [Y/n] ")
+    if repeatstr == "Y" or repeatstr == "y" or repeatstr == '':
+        repeat = True
+    elif repeatstr == "N" or repeatstr == "n":
+        repeat = False
+    elif repeatstr == "penguins":
+        print("Penguins are cool! Now, for your answer to the question...")
+    else:
+        print("Give a proper answer or FELL MY WRATH\n")
 
-print("Thankyou for using The Supreme Grade Calculating Being")
+print("\nThankyou for using The Supreme Grade Calculating Being")
 print("Credits go to Atharv Sonwane and those who have taught him")
