cace1643654198935deb5c574121677477d85b59
Atharv Sonwane <manyogiee@gmail.com>
2015-01-16 19:09:55 +0000

    Create times table generator.py

---
+++ school/times table generator.py
@@ -0,0 +1,28 @@
+# Times Table Generator
+# By Atharv Sonwane
+
+# uncomment
+
+'''
+r=int(input("Hey, I can write out times tables for you. Which times table would you like me to show you."))
+m=int(input("Where do you want it to go up to?"))
+c=1
+'''
+
+# code for while loop
+
+'''
+while c<m+1:
+    print(r, "*", c, "=", r*c)
+    c=c+1
+
+print("Thank-you!")
+'''
+
+# code for for loop
+'''
+for c in range(0,m+1):
+    print(r, "*", c, "=", r*c)
+    c=c+1
+    '''
+
